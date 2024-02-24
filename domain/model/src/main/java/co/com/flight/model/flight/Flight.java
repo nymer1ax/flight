a490cd07934185c4b92efcc72fb6441de2efebf1
Ralph <ralphsliger1@gmail.com>
2024-02-23 19:25:39 -0500

    modelo dominio

--- domain/model/src/main/java/co/com/flight/model/flight/Flight.java
+++ domain/model/src/main/java/co/com/flight/model/flight/Flight.java
@@ -2,13 +2,23 @@ package co.com.flight.model.flight;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class Flight {
+    private String airline;
+    private BigDecimal price;
+    private String cabinClass;
+    private String departureAirportCode;
+    private String destinationAirportCode;
+    private LocalDateTime departureDateTime;
+    private LocalDateTime returnDateTime;
 }
