f640d06fe79411486c891039824cc6c30f251158
Ralph <ralphsliger1@gmail.com>
2024-02-23 19:20:21 -0500

    Initial commit

---
+++ applications/app-service/src/main/java/co/com/flight/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.flight.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.flight.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
